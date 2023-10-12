8e471fb64f71af9b7d96c7123f1e6985aa77e21c
Juaness06 <jesteban.granada@javeriana.edu.co>
2023-10-12 00:16:51 -0500

    la primera parte de lo de los mapas

--- app/src/main/java/com/example/taller2/MapasActivity.kt
+++ app/src/main/java/com/example/taller2/MapasActivity.kt
@@ -1,107 +1,106 @@
 package com.example.taller2
 
-
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Geocoder
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.example.taller2.databinding.ActivityMapasBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import com.example.taller2.databinding.ActivityMapas2Binding
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.MapStyleOptions
+import java.io.IOException
 
 class MapasActivity : AppCompatActivity(), OnMapReadyCallback {
 
-
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapasBinding
+    private lateinit var binding: ActivityMapas2Binding
     private lateinit var sensorManager: SensorManager
     private lateinit var lightSensor: Sensor
     private lateinit var lightSensorListener: SensorEventListener
+    private lateinit var locationManager: LocationManager
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        binding = ActivityMapasBinding.inflate(layoutInflater)
+
+        binding = ActivityMapas2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        //Sensor de luz
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-
-        lightSensorListener = object : SensorEventListener {
-            override fun onSensorChanged(event: SensorEvent) {
-                if (mMap != null)
-                    if (event.values[0] < 5000) {
-                        Log.i(" MAPS", "DARK MAP " + event.values[0])
-                        mMap.setMapStyle(
-                            MapStyleOptions.loadRawResourceStyle(
-                                this@MapasActivity,
-                                R.raw.style_json2
-                            )
-                        )
-                    } else {
-                        Log.i(" MAPS", "LIGHT MAP " + event.values[0])
-                        mMap.setMapStyle(
-                            MapStyleOptions.loadRawResourceStyle(
-                                this@MapasActivity,
-                                R.raw.style_json
-                            )
-                        )
-                    }
-            }
-
-            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-                TODO("Not yet implemented")
-            }
-        }
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-
-     */
 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-
-        //unicamente para que se vea el mapa
-        val Ujaveriana = LatLng(4.6287662, -74.0636298647595)
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f))
-        mMap.addMarker(
-            MarkerOptions().position(Ujaveriana).title("Marker in Universidad Javeriana")
-                .snippet("Aqui Estudio :)")
-        )
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Ujaveriana))
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this@MapasActivity, R.raw.style_json))
 
 
-        mMap.uiSettings.isZoomControlsEnabled =
-            true // Habilitar los "gestures" como "pinch to zoom"
+        //marcar mi posision actual
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                val latitud = location.latitude
+                val longitud = location.longitude
+
+                //Poner un marcador en la ubicación del usuario
+                val miUbicacion = LatLng(latitud, longitud)
+                mMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f))
+                mMap.addMarker(MarkerOptions().position(miUbicacion).title("Mi ubicación"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(miUbicacion))
+            }
+        }
+
+
+
+        mMap.uiSettings.isZoomControlsEnabled = true // Habilitar los "gestures" como "pinch to zoom"
         mMap.uiSettings.isZoomGesturesEnabled = true // Habilitar los botones de zoom
     }
 
 }
+
+
+
+
